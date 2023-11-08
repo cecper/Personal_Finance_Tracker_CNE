1a6da388034fdb814e1c16354f985a4d46330953
SiebeSterckx <siebe.sterckx@student.ucll.be>
2023-11-08 14:08:15 +0100

    test gitignore

--- backend/CreatePiggybankHttpTrigger/index.ts
+++ backend/CreatePiggybankHttpTrigger/index.ts
@@ -1,23 +1,23 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import {Piggybank} from "../domain/model/piggybank";
-import {piggybankServices} from "../domain/service/piggybank.services";
+import { Piggybank } from "../domain/model/piggybank";
+import { piggybankServices } from "../domain/service/piggybank.services";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-/*
-    router.post('/create', async function (req: any, res: any, next: any) {
-
-        try {
-            const piggyBank = new Piggybank(req.body.name, req.body.balance, "");
-            const result = await piggybankServices.createPiggyBank(piggyBank, req.body.username);
-
-            res.status(200).json(result);
-        }
-        catch (error) {
-            res.status(400).json({message: 'Piggybank already exists'});
-        }
-
-    });*/
-    try{
+    /*
+        router.post('/create', async function (req: any, res: any, next: any) {
+    
+            try {
+                const piggyBank = new Piggybank(req.body.name, req.body.balance, "");
+                const result = await piggybankServices.createPiggyBank(piggyBank, req.body.username);
+    
+                res.status(200).json(result);
+            }
+            catch (error) {
+                res.status(400).json({message: 'Piggybank already exists'});
+            }
+    
+        });*/
+    try {
         const piggyBank = new Piggybank(req.body.name, req.body.balance, "");
         const result = await piggybankServices.createPiggyBank(piggyBank, req.body.username);
 
@@ -28,15 +28,15 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
             }
         }
     }
-        catch (error) {
-            context.res = {
-                status: 400,
-                headers: {
-                    "Content-Type": "application/json" 
-                },
-                body: {
-                    message: "user already exists"
-                }
+    catch (error) {
+        context.res = {
+            status: 400,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: {
+                message: "user already exists"
+            }
         }
     }
 
