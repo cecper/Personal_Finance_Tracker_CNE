dac3221355149197e7a54a89dfa9106c3ebb22df
cecper <casper.olijslagers@student.ucll.be>
2023-12-19 16:19:15 +0100

    caching fix transaction delete

--- backend/deleteTransactionByIdHttpTrigger/index.ts
+++ backend/deleteTransactionByIdHttpTrigger/index.ts
@@ -1,8 +1,16 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { transactionsServices } from "../domain/service/transactions.services";
+import {LinkCache} from "../domain/data-access/redis-link-cache";
+import {piggybankServices} from "../domain/service/piggybank.services";
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     try {
         const resp= await transactionsServices.deleteTransactionById(req.body.transactionId,req.body.piggybankId,req.body.userName);
+        const cache = await LinkCache.getInstance();
+
+        const piggyBanks = await piggybankServices.getAllPiggyBanks(req.body.username);
+        const json=JSON.stringify(piggyBanks);
+        await cache.resetPiggybank(req.body.username, json);
+
         context.res = {
             body: resp,
             headers: {
